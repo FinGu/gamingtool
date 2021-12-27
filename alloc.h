dc64bf4bbe6b182d7b7fb4acd447c3739f117423
Fabio <50637180+FinGu@users.noreply.github.com>
2021-12-27 18:54:13 -0300

    Delete alloc.h

--- alloc.h
+++
@@ -1,9 +0,0 @@
-#include "config.h"
-
-void *scalloc(size_t, size_t); //safe calloc
-
-string salloc(size_t); //string alloc
-
-void sfree(string); //string free
-
-char *copycatalloc(size_t, char*, char*);
